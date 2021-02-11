5fab2aec11d40c98625eed6d404e922f5ab3cdb7
DESKTOP-IEGTRSN\Choi InJun <inmonking@gmail.com>
2021-02-11 20:08:31 +0900

    gitignore 수정 경로 이름 오타 수정

--- src/main/java/com/covidplus/configuration/ResourceConfig.java
+++ src/main/java/com/covidplus/configuration/ResourceConfig.java
@@ -1,4 +1,4 @@
-package com.covidplus.configration;
+package com.covidplus.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
